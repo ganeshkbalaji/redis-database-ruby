3c652f1b7a42fce44c2a2021e1197022f7a50411
Ganesh Balaji (C) <gbalaji@splunk.com>
2016-07-11 23:28:08 -0700

    completed challenge

--- readme.md
+++ readme.md
@@ -1,4 +1,5 @@
 <h1>Instructions on how to run</h1>
 
-<p>commands ruby turo</p>
-<p></p>
+<p>commands ruby turo challenge</p>
+<p>For interactive mode use 'ruby turo_database.rb'</p>
+<p>To take input files from the command line use 'ruby turo_database.rb input1.txt'</p>
--- turo_database.rb
+++ turo_database.rb
@@ -1,37 +1,64 @@
 class TuroDatabase
 
-  attr_accessor :database, :transactions, :transaction_count
+  attr_accessor :database, :transactions, :transaction_count, :database_number_equal_to, :transaction_number_equal_to
 
   def initialize
     self.database = {}
     self.transactions = {}
     self.transaction_count = 0
-    self.database_number_equal_to = Hash.new()
-    self.transaction_number_equal_to = Hash.new()
+    self.database_number_equal_to = Hash.new(0)
+    self.transaction_number_equal_to = Hash.new(0)
   end
 
+  #set the variable name to the value, neither variable names or value will contain spaces
   def set(name, value)
+    update(name, value)
   end
 
+  #unset the variable name, so that the variable is never set
   def unset(name)
+    update(name, nil)
   end
 
+  #print out the value of the variable name or null if that variable is not set
   def get(name)
+    name_in_transaction?(name) ? get_value_from_deepest_transaction(name) : self.database[name]
   end
 
+  #print out the number of variables that are currently set to value.
   def num_equal_to(value)
+    if in_transaction?
+      self.transaction_number_equal_to[value] + self.database_number_equal_to[value] ||= 0
+    else
+      self.database_number_equal_to[value] ||= 0
+    end
   end
 
-  def end
-  end
-
+  # open a new transaction block.
   def begin
+    self.transaction_count += 1
   end
 
-  def rollback
+  def rollback()
+    self.transactions.each do |name, trans|
+      last = self.get(name)
+      trans.delete_if { |k| k[0] == self.transaction_count }
+      self.transactions.delete(name) if trans.empty?
+      self.update_num_equal_to(last, self.get(name))
+    end
+    self.transaction_count -= 1
+    self.transactions = {} if self.transaction_count == 0
   end
 
   def commit
+    self.transactions.each do |key, value|
+      value.sort_by { |trans| trans[0] }.each do |ordered|
+        # "ordered #{ordered}"
+        update key, ordered[1], true
+      end
+    end
+    self.transactions = {}
+    self.transaction_count = 0
   end
 
   def process_input
@@ -66,6 +93,66 @@ class TuroDatabase
     }
   end
 
-  def validate_operation(command, options=nil)
+  def update(name, value, commit = false)
+    current = self.get(name)
+
+    #if in a transaction, create the empty array if value is nil
+    self.transactions[name] ||= [] unless self.transaction_count.zero?
+    if !in_transaction? || commit
+      self.database[name] = value unless self.database[name] == value
+    elsif in_transaction?
+      transaction_value = [self.transaction_count, value]
+      self.transactions[name] << transaction_value unless self.transactions[name].include? transaction_value
+    elsif value.nil?
+      self.database.delete_if { |val| val == name } #not in transaction / delete from database
+    end
+    update_num_equal_to(current, value)
   end
+
+  def update_num_equal_to(current, new=nil)
+    [[-1, current], [1, new]].each do |counter_operator, value|
+      if in_transaction?
+        self.transaction_number_equal_to[value] += counter_operator
+      else
+        self.database_number_equal_to[value] += counter_operator
+      end unless value.nil?
+    end
+  end
+
+  def validate_operation(command, options=nil)
+    all_required_present = options[:required].values.all? { |req| !req.nil? }
+    unless all_required_present
+      puts "INVALID OPERATION. #{command.upcase} requires: (#{options[:required].keys.join(', ')})"
+      FALSE
+    else
+      TRUE
+    end
+  end
+
+  def display_result(value)
+    puts value.nil? ? 'NULL' : value
+  end
+
+  def get_value_from_deepest_transaction(name)
+    value = self.transactions[name][-1]
+    if value
+      value[1]
+    end
+  end
+
+  def in_transaction?
+    self.transaction_count > 0;
+  end
+
+  def name_in_transaction?(name)
+    self.transactions[name]
+  end
+
+  def method_missing(m, *args, &block)
+    puts "WARNING: '#{m}' is not a recognized command"
+  end
+
 end
+
+turo_db = TuroDatabase.new
+turo_db.process_input
